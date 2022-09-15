7c550f792f880937ecc474097e52c02317a6fbf5
Yohans Nascimento <yohans.dev@gmail.com>
2022-09-15 16:16:46 -0300

    Upload Exercicio4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta) ) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta) ) / (2 * a);
+            
+            if(delta == 0)
+            {
+                Console.WriteLine(x1);
+            }
+            else if(delta > 0)
+            {
+                Console.WriteLine($"X1: {Math.Truncate(x1)}");
+                Console.WriteLine($"X2: {Math.Truncate(x2)}");
+            }
+            else
+            {
+                Console.WriteLine("Não possui raiz");
+            }
+
+            //Delta = b^2 - 4.a.c
+            //-b +- VDelta / 2.a
+
+        }
+    }
+}

a3e41294c31bddb9265791e15675b3a30074c879
Yohans Nascimento <yohans.dev@gmail.com>
2022-09-19 16:11:28 -0300

    Upload Exercicio5 BankAccount

---
+++ Exercicio5/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio5
+{
+    internal class BankAccount
+    {
+        public long Codigo { get; set; }
+        public string NomeProprietario { get; set; }
+        public double Saldo { get; set; }
+
+        public void Deposito(double valorDepositado)
+        {
+            Saldo += valorDepositado;
+            Console.WriteLine($"O valor depositado foi: R${valorDepositado} \nSaldo total da conta: R${Saldo}.");
+        }
+        public void Saques(double valorSacado)
+        {
+            Saldo -= valorSacado;
+            Console.WriteLine($"O valor sacado foi: R${valorSacado} \nSaldo total da conta: R${Saldo}.");
+        }
+    }
+}
---
+++ Exercicio5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var banco = new BankAccount();
+            banco.Deposito(100);
+        }
+    }
+}

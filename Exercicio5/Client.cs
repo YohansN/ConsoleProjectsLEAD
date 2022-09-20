512c2c4e3b57c3f217d8b8e627dff9469a06b21f
Yohans Nascimento <yohans.dev@gmail.com>
2022-09-20 15:26:57 -0300

    Upload Client Class

---
+++ Exercicio5/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio5
+{
+    internal class Client
+    {
+        public string Nome { get; set; }
+        public string Ocupacao { get; set; }
+        public string DataNascimento { get; set; } 
+        public string Email { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine($"Nome: {Nome}\nOcupação: {Ocupacao}\nData de nascimento: {DataNascimento}\nEmail: {Email}");
+        }
+
+        public void Age()
+        {
+            DateTime dataNascimento = Convert.ToDateTime(this.DataNascimento);
+            DateTime dataAtual = DateTime.Now;
+
+            int anoAtual = dataAtual.Year;
+            int anoNascimento = dataNascimento.Year;
+
+            int age = anoAtual - anoNascimento;
+            
+            Console.WriteLine($"Idade do cliente: {age} anos.");
+        }
+
+        public Client(string nome, string ocupacao, string dataNascimento, string email)
+        {
+            Nome = nome;
+            Ocupacao = ocupacao;
+            DataNascimento = dataNascimento;
+            Email = email;
+        }
+    }
+}
--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -6,7 +6,11 @@ namespace Exercicio5
     {
         static void Main(string[] args)
         {
-            
+            Client cliente = new Client("Yohans","Programador","01/10/2002","yohans@gmail.com");
+
+            cliente.Info();
+
+            cliente.Age();
             
         }
     }
